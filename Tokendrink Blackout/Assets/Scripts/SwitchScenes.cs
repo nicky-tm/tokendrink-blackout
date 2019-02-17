45b6adf20efd0cb5b825e4b75fd4311d6babaad3
nicky-tm <32135596+nicky-tm@users.noreply.github.com>
2019-02-17 11:54:47 +0100

    Add project

---
+++ Tokendrink Blackout/Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Alpha0))
+        {
+           StartCoroutine(LoadPeopleCounter());
+        } else if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+           StartCoroutine(LoadCountdown());
+        } else if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+           StartCoroutine(LoadCurrentScore());
+        }
+    }
+
+    IEnumerator LoadPeopleCounter()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("PeopleCounter");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    IEnumerator LoadCountdown()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Countdown");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    IEnumerator LoadCurrentScore()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("CurrentScore");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
